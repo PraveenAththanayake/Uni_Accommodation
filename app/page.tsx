66e84f60fe75237b79e4616872328722fc2febb6
MarkDillonJ <115799520+MarkDillonJ@users.noreply.github.com>
2024-04-07 12:07:46 +0530

    admin page UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
+import UserInfo from "./(roles)/admin/page";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -11,12 +12,16 @@ const font = Poppins({
 
 export default function Home() {
   return (
+    /*
     <main className={cn("h-full flexCenter flex-col", font.className)}>
       <LoginButton>
         <Button variant="secondary" size="lg">
           Get Started
         </Button>
       </LoginButton>
-    </main>
+    </main>*/
+
+    <UserInfo/>
+    
   );
 }
